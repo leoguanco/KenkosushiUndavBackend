276cf4b1e0cda4cd73c1a2289e57dd20f2137f30
Leo Guanco <leo.guanco@gmail.com>
2017-11-07 13:11:13 -0300

    Proyecto completo

---
+++ src/main/java/com/kenkosushi/Undav/UndavApplication.java
@@ -0,0 +1,12 @@
+package com.kenkosushi.Undav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UndavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UndavApplication.class, args);
+	}
+}
