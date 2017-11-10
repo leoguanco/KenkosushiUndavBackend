7c8ba59be6b5116886e526cb1d10561858eefc31
Leo Guanco <leo.guanco@gmail.com>
2017-11-09 21:54:07 -0300

    Repositorio de address

--- src/main/java/com/kenkosushi/Undav/domain/repository/AddressRepository.java
+++ src/main/java/com/kenkosushi/Undav/domain/repository/AddressRepository.java
@@ -1,7 +1,10 @@
 package com.kenkosushi.Undav.domain.repository;
 
+import com.kenkosushi.Undav.domain.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * Created by leo on 11/9/17.
  */
-public class AddressRepository {
+public interface AddressRepository extends JpaRepository<Address,Long>{
 }
