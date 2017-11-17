49f1553121559b3f0714e98e271ef91a21babf85
ima <imanollew@hotmail.com>
2017-11-17 20:33:23 -0300

    modificado el PurchaseController

--- src/main/java/com/kenkosushi/Undav/controller/PurchaseController.java
+++ src/main/java/com/kenkosushi/Undav/controller/PurchaseController.java
@@ -1,25 +1,33 @@
 package com.kenkosushi.Undav.controller;
 
 import com.kenkosushi.Undav.domain.model.Purchase;
+import com.kenkosushi.Undav.domain.model.PurchaseProduct;
+import com.kenkosushi.Undav.service.PurchaseProductService;
 import com.kenkosushi.Undav.service.PurchaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 
 public class PurchaseController {
 
     private PurchaseService purchaseService;
+    private PurchaseProductService purchaseProductService;
+    private JavaMailSender mailSender;
 
     @Autowired
-    public PurchaseController (PurchaseService purchaseService){
+    public PurchaseController (PurchaseService purchaseService,PurchaseProductService purchaseProductService){
         this.purchaseService=purchaseService;
+        this.purchaseProductService=purchaseProductService;
     }
 
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping("/admin/purchases")
-    public ResponseEntity<Purchase> addPurchase(Purchase purchase){
+    public ResponseEntity<Purchase> addPurchase(Purchase purchase, PurchaseProduct purchaseProduct){
+        purchaseProductService.save(purchaseProduct);
         purchaseService.save(purchase);
         return new ResponseEntity<Purchase>(purchase, HttpStatus.CREATED);
     }
