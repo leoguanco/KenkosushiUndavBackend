e92c20781a4636609727c48c96db5f8d77d6fc16
Leo Guanco <leo.guanco@gmail.com>
2017-11-25 00:57:00 -0300

    Configuraciones del servicio

--- src/main/java/com/kenkosushi/Undav/service/UserService.java
+++ src/main/java/com/kenkosushi/Undav/service/UserService.java
@@ -1,18 +1,21 @@
 package com.kenkosushi.Undav.service;
 
+import com.kenkosushi.Undav.domain.model.Role;
 import com.kenkosushi.Undav.domain.model.User;
 import com.kenkosushi.Undav.domain.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
+import java.util.*;
 
 import static java.util.Collections.emptyList;
 
-@Service
+@Service("userService")
 public class UserService implements UserDetailsService{
 
     private UserRepository userRepository;
@@ -45,10 +48,27 @@ public class UserService implements UserDetailsService{
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(s);
+        List<GrantedAuthority> authorities =  buildAuthorities(user.getRole());
+
         if(user == null){
             throw new UsernameNotFoundException(s);
         }
 
-        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(), emptyList());
+        return buildUser(user, authorities);
+    }
+
+    private org.springframework.security.core.userdetails.User buildUser(User user, List<GrantedAuthority> authorities){
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), user.getActive(),
+                true, true, true, authorities);
+    }
+
+    private List<GrantedAuthority> buildAuthorities(Set<Role> roles){
+        Set<GrantedAuthority> authorities = new HashSet<GrantedAuthority>();
+
+        for(Role role: roles){
+            authorities.add(new SimpleGrantedAuthority(role.getRole()));
+        }
+
+        return new ArrayList<GrantedAuthority>(authorities);
     }
 }
