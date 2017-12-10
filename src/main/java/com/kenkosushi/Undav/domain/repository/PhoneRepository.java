702bdefe5fb105edbadf55b862b3fe18646fde5c
Leo Guanco <leo.guanco@gmail.com>
2017-12-10 19:00:21 -0300

    Agregados endpoints para eliminar todos los telefonos o direcciones de un usuario

--- src/main/java/com/kenkosushi/Undav/domain/repository/PhoneRepository.java
+++ src/main/java/com/kenkosushi/Undav/domain/repository/PhoneRepository.java
@@ -1,11 +1,12 @@
 package com.kenkosushi.Undav.domain.repository;
 
 import com.kenkosushi.Undav.domain.model.Phone;
+import com.kenkosushi.Undav.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Collection;
 
 public interface PhoneRepository extends JpaRepository<Phone,Long>{
 
-    public Collection<Phone> findByUserId(Long id);
+    public Collection<Phone> findByUser(User user);
 }
